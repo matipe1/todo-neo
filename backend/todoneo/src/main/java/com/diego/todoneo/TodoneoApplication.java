6eac148cbb7e1315631dcfeace2de5e63b8c7541
Diego Petitto <dmpetitto2004@gmail.com>
2025-12-04 12:15:26 -0300

    Add spring boot initializr

---
+++ backend/todoneo/src/main/java/com/diego/todoneo/TodoneoApplication.java
@@ -0,0 +1,13 @@
+package com.diego.todoneo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoneoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoneoApplication.class, args);
+	}
+
+}
---
+++ backend/todoneo/src/test/java/com/diego/todoneo/TodoneoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diego.todoneo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoneoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
