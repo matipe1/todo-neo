d23f8e06d9aadad6533c38af4b59a2aa484decd2
Diego Petitto <dmpetitto2004@gmail.com>
2025-12-07 22:18:12 -0300

    Add basic global exception handler implementation

---
+++ backend/todoneo/src/main/java/com/diego/todoneo/utils/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.diego.todoneo.utils.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleGeneralPurposeException(Exception ex) {
+        return new ResponseEntity<>("Unexpected error ocurred: " + ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<String> handleResourceNotFoundException(ResourceNotFoundException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+}
